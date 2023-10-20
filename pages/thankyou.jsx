3d2df83cc5a89ab79e18657626a2f5a1b58fa989
0xShikhar <0xshikhar@gmail.com>
2023-10-20 10:02:43 +0530

    added pages

---
+++ pages/thankyou.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <div>
+      <h1 className="font-[700] text-white text-2xl flex flex-row justify-center content-center items-center p-10 m-16" >
+        Thank you for Using our platform for hosting tournaments,we hope to see
+        you soon!
+      </h1>
+    </div>
+  );
+};
+
+export default ThankYou;
